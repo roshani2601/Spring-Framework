aacd9aa7169d1b0d551fef9703fdf975948de60f
roshani2601 <borasadiyaroshani@gmail.com>
2024-10-17 06:04:09 +0530

    Main Class : Which can pull the object and use

--- SpringCore/src/main/java/com/springcore/Student.java
+++ SpringCore/src/main/java/com/springcore/Student.java
@@ -4,22 +4,26 @@ public class Student {
 	private int studentId;
 	private String studentName;
 	private String studentAddress;
+	
 	public int getStudentId() {
 		return studentId;
 	}
 	public void setStudentId(int studentId) {
+		System.out.println("Setting Student ID.");
 		this.studentId = studentId;
 	}
 	public String getStudentName() {
 		return studentName;
 	}
 	public void setStudentName(String studentName) {
+		System.out.println("Setting Student Name.");
 		this.studentName = studentName;
 	}
 	public String getStudentAddress() {
 		return studentAddress;
 	}
 	public void setStudentAddress(String studentAddress) {
+		System.out.println("Setting Student Address.");
 		this.studentAddress = studentAddress;
 	}
 	
