5f390632a56adb89fb3d2b4cb0d3d842a8e7d17b
roshani2601 <borasadiyaroshani@gmail.com>
2024-11-20 15:33:27 +0530

    Without XML file Spring configuration

---
+++ SpringCore/src/main/java/com/springcore/javaconfig/Student.java
@@ -0,0 +1,11 @@
+package com.springcore.javaconfig;
+
+import org.springframework.stereotype.Component;
+
+
+public class Student {
+
+	public void study() {
+		System.out.println("Students are studing");
+	}
+}
---
+++ SpringCore/src/main/java/com/springcore/javaconfig/Test.java
@@ -0,0 +1,17 @@
+package com.springcore.javaconfig;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+		Student s1 = context.getBean("getStudent", Student.class);
+		System.out.println(s1);
+		s1.study();
+
+	}
+
+}
